3c40d455ff40dc2b1c0b6eae4a093765b1dabfd0
Zain Sadaqat <zain.sadaqet@gmail.com>
2021-12-24 00:32:13 +0500

    Decode morse code message

--- main.rb
+++ main.rb
@@ -1,3 +1,32 @@
+MORSE_CODE = {
+	".-" =>"a" ,
+  "-..." =>"b" ,
+	"-.-." =>"c" ,
+	"-.." =>"d" ,
+	"." =>"e" ,
+	"..-." =>"f" ,
+	"--." =>"g" ,
+	"...." =>"h" ,
+	".." =>"i" ,
+	".---" =>"j" ,
+	"-.-" =>"k" ,
+	".-.." =>"l" ,
+	"--" =>"m", 
+	"-." =>"n", 
+	"---" =>"o", 
+	".--." =>"p" ,
+	"--.-" =>"q" ,
+	".-." =>"r", 
+	"..." =>"s", 
+	"-" =>"t", 
+	"..-" =>"u", 
+	"...-" =>"v",
+	".--" =>"w", 
+	"-..-" =>"x",
+	"-.--" =>"y" ,
+	"--.." => "z",
+}
+
 def decode_word(morse_code)
   if morse_code == ".-"
     "A"
@@ -16,6 +45,18 @@ def decode_word_3(morse_code)
   end
 end
 
+def decode_word4
+  message = '      .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-. / .-. ..- -... .. . ...'
+  words = message.strip.split('   ')
+  chrs = words.join(' ').split(' ')
+  characters = []
+  chrs.each do |i|
+    print MORSE_CODE[i]
+  end
+end
+
 puts decode_word(".-")
 puts decode_word_2("-- -.--")
 puts decode_word_3("-- -.--   -. .- -- .")
+
+decode_word4
